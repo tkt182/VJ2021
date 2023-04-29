a20901a37a94867bd18a078d5a17c881dcb37af8
shinichiro takata <shinichiro.takata@gmail.com>
2023-04-30 07:55:32 +0900

    Update Scene1

--- Assets/Scenes/Scene1/Phyllotaxis.cs
+++ Assets/Scenes/Scene1/Phyllotaxis.cs
@@ -40,20 +40,34 @@ public class Phyllotaxis : MonoBehaviour
     }
     private Vector2 _phyllotaxisPosition;
 
+    private bool _forward;
+    public bool _repeat, _invert;
+
+    // Scaling
+    public bool _useScaleAnimation, _useCaleCurve;
+    public Vector2 _scaleAnimMinMax;
+    public AnimationCurve _scaleAnimCurve;
+    public float _scaleAnimSpeed;
+    public int _scaleBand;
+    private float _scaleTimer, _currentScale;
+
+
     void SetLerpPositions() {
-        _phyllotaxisPosition = CalculatePhyllotaxis(_degree, _scale, _number);
+        _phyllotaxisPosition = CalculatePhyllotaxis(_degree, _currentScale, _number);
         _startPosition = this.transform.localPosition;
         _endPosition = new Vector3(_phyllotaxisPosition.x, _phyllotaxisPosition.y, 0);
     }
 
 
     void Awake() {
+        _currentScale = _scale;
+        _forward = true;
         _trailRenderer = GetComponent<TrailRenderer>();
         _trailMat = new Material(_trailRenderer.material);
         _trailMat.SetColor("_TintColor", _trailColor);
         _trailRenderer.material = _trailMat;
         _number = _numberStart;
-        transform.localPosition = CalculatePhyllotaxis(_degree, _scale, _number);
+        transform.localPosition = CalculatePhyllotaxis(_degree, _currentIteration, _number);
         if (_useLerping) {
             _isLerping = true;
             SetLerpPositions();
@@ -65,6 +79,19 @@ public class Phyllotaxis : MonoBehaviour
     }
 
     private void Update() {
+
+        if(_useCaleCurve) {
+            if(_useCaleCurve) {
+                _scaleTimer += (_scaleAnimSpeed * _controlParameters._rawAudio[_scaleBand]) * Time.deltaTime;
+                if (_scaleTimer >= 1) {
+                    _scaleTimer -= 1;
+                }
+                _currentScale = Mathf.Lerp(_scaleAnimMinMax.x, _scaleAnimMinMax.y, _scaleAnimCurve.Evaluate(_scaleTimer));
+            } else {
+                _currentScale = Mathf.Lerp(_scaleAnimMinMax.x, _scaleAnimMinMax.y, _controlParameters._rawAudio[_scaleBand]);
+            }
+        }
+
         if (_useLerping) {
             if (_isLerping) {
 
@@ -77,45 +104,37 @@ public class Phyllotaxis : MonoBehaviour
                 transform.localPosition = Vector3.Lerp(_startPosition, _endPosition, Mathf.Clamp01(_lerpPosTimer));
                 if (_lerpPosTimer >= 1) {
                     _lerpPosTimer -= 1;
-                    _number += _stepSize;
-                    _currentIteration++;
-                    SetLerpPositions();
-                }
-            }
-        } else {
-            _phyllotaxisPosition = CalculatePhyllotaxis(_degree, _scale, _number);
-            transform.position = new Vector3(_phyllotaxisPosition.x, _phyllotaxisPosition.y, 20);
-            _number += _stepSize;
-            _currentIteration++;
-        }
-    }
-
-
-    /*
-    private void FixedUpdate() {
-        if (_useLerping) {
-            if (_isLerping) {
-                float timeSinceStated = Time.time - _timeStartedLerping;
-                float percentageComplete = timeSinceStated / _intervalLerp;
-                transform.localPosition = Vector3.Lerp(_startPosition, _endPosition, percentageComplete);
-                if (percentageComplete >= 0.97f) {
-                    transform.localPosition = _endPosition;
-                    _number += _stepSize;
-                    _currentIteration++;
-                    if (_currentIteration <= _maxIteration) {
-                        StartLerping();
+                    if (_forward) {
+                        _number += _stepSize;
+                        _currentIteration++;
                     } else {
-                        _isLerping = false;
+                        _number -= _stepSize;
+                        _currentIteration--;
+                    }
+                    if ((_currentIteration > 0) && (_currentIteration < _maxIteration)) {
+                        SetLerpPositions();
+                    } else { // current iteration has hit 0 or maxiteration
+                        if (_repeat) {
+                            if (_invert) {
+                                _forward = !_forward;
+                                SetLerpPositions();
+                            } else {
+                                _number = _numberStart;
+                                _currentIteration = 0;
+                                SetLerpPositions();
+                            }
+                        } else {
+                            _isLerping = false;
+                        }
                     }
                 }
             }
         } else {
-            _phyllotaxisPosition = CalculatePhyllotaxis(_degree, _scale, _number);
+            _phyllotaxisPosition = CalculatePhyllotaxis(_degree, _currentScale, _number);
             transform.position = new Vector3(_phyllotaxisPosition.x, _phyllotaxisPosition.y, 20);
             _number += _stepSize;
             _currentIteration++;
-
         }
     }
-    */
+
 }
